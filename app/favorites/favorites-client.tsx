ba1646aa34c0574b77867b3ec9e5dae76b65c79f
FjrREPO <jossfajar21@gmial.com>
2023-11-15 11:00:18 +0700

    41

--- app/favorites/favorites-client.tsx
+++ app/favorites/favorites-client.tsx
@@ -2,7 +2,7 @@ import { SafeListing, SafeUser } from "@/app/types"
 
 import Heading from "@/app/components/heading"
 import Container from "@/app/components/container"
-import ListingCard from "@/app/components/listings/listing-card"
+import ListingCardReserve from "../components/listings/listing-cardReserve"
 
 interface FavoritesClientProps {
     listings: SafeListing[]
@@ -22,7 +22,7 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({
             <div
                 className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
                 {listings.map((listing: any) => (
-                    <ListingCard
+                    <ListingCardReserve
                         currentUser={currentUser}
                         key={listing.id}
                         data={listing}
