39095d196b6411515ab11b71a0043b220d7101b9
FjrREPO <jossfajar21@gmial.com>
2023-11-15 11:06:16 +0700

    42

--- app/properties/properties-client.tsx
+++ app/properties/properties-client.tsx
@@ -9,7 +9,7 @@ import { SafeListing, SafeUser } from "@/app/types"
 
 import Heading from "@/app/components/heading"
 import Container from "@/app/components/container"
-import ListingCard from "@/app/components/listings/listing-card"
+import ListingCardReserve from "../components/listings/listing-cardReserve"
 
 interface PropertiesClientProps {
     listings: SafeListing[]
@@ -48,7 +48,7 @@ const PropertiesClient: React.FC<PropertiesClientProps> = ({
             <div
                 className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
                 {listings.map((listing: any) => (
-                    <ListingCard
+                    <ListingCardReserve
                         key={listing.id}
                         data={listing}
                         actionId={listing.id}
